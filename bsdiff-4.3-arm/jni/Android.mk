d8778df35d970ca9a301905613e86fc4d79b0593
qtt <qintangtao@apkshow.com>
2018-01-17 17:41:19 +0800

    add bsdiff-4.3-arm

---
+++ bsdiff-4.3-arm/jni/Android.mk
@@ -0,0 +1,25 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := bsdiff
+LOCAL_SRC_FILES :=  bsdiff.c xmd5.c \
+					../../bzip2-1.0.6/blocksort.c \
+					../../bzip2-1.0.6/bzlib.c \
+					../../bzip2-1.0.6/compress.c \
+					../../bzip2-1.0.6/decompress.c \
+					../../bzip2-1.0.6/crctable.c \
+					../../bzip2-1.0.6/randtable.c \
+					../../bzip2-1.0.6/huffman.c
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := bspatch
+LOCAL_SRC_FILES :=  bspatch.c xmd5.c \
+					../../bzip2-1.0.6/blocksort.c \
+					../../bzip2-1.0.6/bzlib.c \
+					../../bzip2-1.0.6/compress.c \
+					../../bzip2-1.0.6/decompress.c \
+					../../bzip2-1.0.6/crctable.c \
+					../../bzip2-1.0.6/randtable.c \
+					../../bzip2-1.0.6/huffman.c
+include $(BUILD_EXECUTABLE)
