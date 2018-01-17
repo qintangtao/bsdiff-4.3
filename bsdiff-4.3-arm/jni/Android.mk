06b3694dc0062e02bd13372c4aaf1b3f1fa8e2fe
qtt <qintangtao@apkshow.com>
2018-01-17 17:57:28 +0800

    add -fPIE

--- bsdiff-4.3-arm/jni/Android.mk
+++ bsdiff-4.3-arm/jni/Android.mk
@@ -10,6 +10,8 @@ LOCAL_SRC_FILES :=  bsdiff.c xmd5.c \
 					../../bzip2-1.0.6/crctable.c \
 					../../bzip2-1.0.6/randtable.c \
 					../../bzip2-1.0.6/huffman.c
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
@@ -22,4 +24,6 @@ LOCAL_SRC_FILES :=  bspatch.c xmd5.c \
 					../../bzip2-1.0.6/crctable.c \
 					../../bzip2-1.0.6/randtable.c \
 					../../bzip2-1.0.6/huffman.c
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 include $(BUILD_EXECUTABLE)
