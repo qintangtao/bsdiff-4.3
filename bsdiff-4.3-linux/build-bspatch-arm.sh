b6ce827eb186bd4ea235100dbf497f403e74cc8b
qtt <qintangtao@apkshow.com>
2018-01-18 16:40:03 +0800

    rm build sh

--- bsdiff-4.3-linux/build-bspatch-arm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-COMPILE=/opt/FriendlyARM/toolschain/4.5.1/bin
-CC=$COMPILE/arm-linux-gcc
-STRIP=$COMPILE/arm-linux-strip
-$CC -I ../bzip2-1.0.6 -O2 xmd5.cpp bspatch.c -o bspatch -L ../bzip2-1.0.6 -l bz2
-$STRIP bspatch
-
